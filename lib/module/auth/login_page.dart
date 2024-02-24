3b49e3e0a3a4f20fbf235c460844b8633a4a4eb3
lenovo <aseemmtk@gmail.com>
2024-02-24 15:20:39 +0530

    login latest1

--- lib/module/auth/login_page.dart
+++ lib/module/auth/login_page.dart
@@ -4,26 +4,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 import '../../widgets/custom_text_field.dart';
 
 class loginpage extends StatelessWidget {
-   loginpage({super.key});
+  loginpage({super.key});
 
   final _emailCntroller = TextEditingController();
   final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
         body: Padding(
-
-
-
-
-
-
-
       padding: const EdgeInsets.all(8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -36,20 +30,59 @@ class loginpage extends StatelessWidget {
             style: TextStyle(fontFamily: 'circular_bold', fontSize: 30),
           ),
           const SizedBox(
-            height: 70,
+            height: 130,
           ),
           CustomTextField(
             controller: _emailCntroller,
             hintText: "Enter email",
           ),
           const SizedBox(
-            height: 50,
+            height: 30,
           ),
           CustomTextField(
             controller: _passwordController,
             hintText: "Enter password",
           ),
-
+          const SizedBox(
+            height: 10,
+          ),
+          const Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              "Forget password",
+              style: TextStyle(fontFamily: 'circular_bold', fontSize: 15),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Center(
+            child: SizedBox(
+              width: 300,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.amber),
+                child:
+                    const Text("LOG IN", style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Center(
+                child: Text(
+                  "Dont have account ?",
+                  style: TextStyle(fontFamily: 'circular_bold', fontSize: 15),
+                ),
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: const Text("Register",
+                      style: TextStyle(color: Colors.amber)))
+            ],
+          ),
         ],
       ),
     ));
